be7214757fde81f1936c608b0dea700c54ca7a65
Nemanja NIkolic <nnikolic.kg@gmail.com>
2024-11-30 16:17:28 -0600

    debug false

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ env: dict = dotenv_values(
 SECRET_KEY = env.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "10.160.9.125"]
 
