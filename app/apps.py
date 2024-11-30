dac4e90fd824c418073376ec3718305662a08d41
Nemanja NIkolic <nnikolic.kg@gmail.com>
2024-11-30 15:05:52 -0600

    implemented running migrations on startup

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+from django.db import connection
+from django.core.management import call_command
 
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+    def ready(self):
+        call_command("migrate", interactive=False)
